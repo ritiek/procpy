2ee2968feb60c8285d1057c5a13e66cec1210da9
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-10-14 03:12:27 +0530

    bugfix: correctly parse names with spaces

--- procpy/process.py
+++ procpy/process.py
@@ -23,8 +23,9 @@ class Bytes:
 
 class Process:
     def __init__(self, pid):
-        self.pid = pid
+        self.pid = int(pid)
         self._process_info = []
+        self._name_offset = 0
 
     def read_stat(self):
         stat_file = os.path.join(procpy.PROC_FS, str(self.pid), procpy.PROC_STAT)
@@ -33,25 +34,37 @@ class Process:
                 content = fin.read()
         except FileNotFoundError:
             if self.pid == 0:
-                raise SwapProcessError("PID 0: unable to read stat for swap process")
+                raise procpy.SwapProcessError("PID 0: unable to read stat for swap process")
             else:
-                raise ProcessNotFoundError('PID {}: "{}" does not exist'.format(
+                raise procpy.ProcessNotFoundError('PID {}: "{}" does not exist'.format(
                     self.pid,
                     os.path.join(procpy.PROC_FS, str(self.pid), procpy.PROC_STAT))
                 )
         self._process_info = content.split()
+        self._name_offset = self._calculate_name_offset()
+
+    def _calculate_name_offset(self):
+        last_name_index = 1
+        for i, value in enumerate(self._process_info[last_name_index:]):
+            if len(value) == 1:
+                break
+            last_name_index += 1
+        name = self._process_info[1:last_name_index]
+        return len(name) - 1
 
     def __repr__(self):
         return 'Process<(pid={})>'.format(self.pid)
 
     @property
     def name(self):
-        return self._process_info[1][1:-1]
+        name = self._process_info[1:self._name_offset + 2]
+        name = " ".join(name)
+        return name[1:-1]
 
     @property
     def parent(self):
-        return Process(self._process_info[3])
+        return Process(self._process_info[3 + self._name_offset])
 
     @property
     def virtual_memory(self):
-        return Bytes(self._process_info[23])
+        return Bytes(self._process_info[23 + self._name_offset])
