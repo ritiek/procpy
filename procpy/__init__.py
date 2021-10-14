94da79a8212c5129b813f63083391088c8c6166e
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-10-14 20:35:41 +0530

    refactor: use absolute imports

--- procpy/__init__.py
+++ procpy/__init__.py
@@ -1,4 +1,4 @@
-from .constants import PROC_FS, PROC_STAT
-from .exceptions import ProcessNotFoundError, SwapProcessError, ReadError
-from .process import Process
-from .procfs import ProcFS
+from procpy.constants import PROC_FS, PROC_STAT
+from procpy.exceptions import ProcessNotFoundError, SwapProcessError, ReadError
+from procpy.process import Bytes, Process
+from procpy.procfs import ProcFS
