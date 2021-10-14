5f5fc2ee6e02f98549d8a5166fa8add6a88740ac
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-10-14 23:48:01 +0530

    test: add tests for bytes

---
+++ procpy/tests/test_bytes.py
@@ -0,0 +1,25 @@
+import procpy
+import unittest
+
+
+class TestBytes(procpy.tests.TestInitialize):
+    def setUp(self):
+        super().setUp()
+        self.bytes = procpy.Bytes(34295)
+
+    def test_human_readable(self):
+        size, unit = self.bytes.human_readable()
+        self.assertEqual((round(size, 2), unit), (33.49, "KB"))
+
+    def test_equals(self):
+        self.assertEqual(self.bytes, procpy.Bytes(34295))
+
+    def test_int_value(self):
+        self.assertEqual(int(self.bytes), 34295)
+
+    def test_repr(self):
+        self.assertEqual(repr(self.bytes), "Bytes<(numeral=34295)>")
+
+
+if __name__ == "__main__":
+    unittest.main()
