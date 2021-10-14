f7f50a1d9075cc6fd3e046ad2825c11026b9f427
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2021-10-14 22:51:04 +0530

    refactor: improve virtual process logic

--- procpy/tests/test_process.py
+++ procpy/tests/test_process.py
@@ -3,8 +3,9 @@ import unittest
 
 
 class TestProcess(procpy.tests.TestInitialize):
-    def test_existing_process(self):
-        pid = 1337
+    def setUp(self):
+        super().setUp()
+        self.pid = 1337
         stat = ("1337 (tmux: client) S 1337 93385 91239 34819 "
                 "93385 4194304 276 0 3 0 1 0 0 0 20 0 1 0 3557720 "
                 "9011200 372 18446744073709551615 93915331723264 "
@@ -12,13 +13,13 @@ class TestProcess(procpy.tests.TestInitialize):
                 "134433283 0 0 0 17 0 0 0 0 0 0 93915332497968 "
                 "93915332569544 93915340509184 140731588121938 "
                 "140731588121945 140731588121945 140731588128746 0")
-        self.create_virtual_process(pid, stat)
+        self.create_virtual_process(self.pid, stat)
 
-        process = procpy.Process(pid)
-        parent_process = procpy.Process(pid)
-
+    def test_existing_process(self):
+        process = procpy.Process(self.pid)
+        parent_process = procpy.Process(self.pid)
         self.assertEqual(process.name, "tmux: client")
-        self.assertEqual(process.parent, parent_process)
+        self.assertEqual(process.parent.pid, parent_process.pid)
         self.assertEqual(process.virtual_memory, procpy.Bytes(9011200))
 
     def test_non_existing_process(self):
